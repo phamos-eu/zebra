c51e415220e535e35dbee3662c4ec37937e21755
Deepak Kumar <deepak@korecent.com>
2023-03-22 12:38:19 +0000

    equipment location update

--- zebra/zebra/doctype/rfid_logs/rfid_logs.py
+++ zebra/zebra/doctype/rfid_logs/rfid_logs.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, phamos and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RFIDLogs(Document):
-	pass
+	def after_insert(self):
+		""" When record is inserted check for the equipment with the rfid no. and update the location field """
+		equipment = frappe.get_doc('Equipment', {'rfid_number': self.id})
+		if equipment.last_location != self.location:
+			equipment.last_location = self.location
+			equipment.save()
